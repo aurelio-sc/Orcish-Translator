568bd9105c7238349e01384e1cdf9f20c6c2fdcb
Aurélio Soares <95449811+aurelio-sc@users.noreply.github.com>
2022-03-13 18:34:29 -0300

    Header created

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script>
 	import BasicTranslator from "./components/BasicTranslator.svelte";
+	import Header from "./components/Header.svelte";
 </script>
 
 <main>
+	<Header/>
 	<BasicTranslator/>
 	
 </main>
---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+<main>
+  <h1>Translate to Orcish!</h1>
+  <p>This translator is meant to be used by role players. it was designed with the Shards of Britannia community in mind</p>
+  <p>Feel free to try it out and, if you like it, sharing among your friends.</p>
+</main>
+
+
+<style>
+  main{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    line-height: 1.6;
+  }
+
+  h1{
+    font-size: 2rem;
+  }
+</style>
